863dea1aa57ffaa8e696c6d9aa18aab401d8316e
sebhs <sebhs@stanford.edu>
2017-12-28 13:26:38 +0100

    Add files via upload

---
+++ flying_copter.js
@@ -0,0 +1,67 @@
+
+/*
+ * A little copter that flies over your console
+ * Author: Sebastianos Hapte-Selassie (sebhs)
+ */
+
+
+var clear = require('clear');
+
+for (let i = 0; i < 5; i++) {
+		let ident = get_ident_string(i)
+
+		clear();
+		console.log("")
+		console.log(ident + ":XXXX:XXXX:OOO:XXXX:XXXX:")
+		console.log(ident + "           ^            ")
+		console.log(ident + "  X   /–––––––––––     ")
+		console.log(ident + "  O  ===       [] \\    ")
+		console.log(ident + "  X    \\           \\  ")
+		console.log(ident + "        \\___________]  ")
+		console.log(ident + "            I     I    ")
+		console.log(ident + "")
+
+		
+		pause(50);	
+		clear();
+		console.log(ident + "")
+		console.log(ident + "             :OOO:XXXX:XXXX:")
+		console.log(ident + "              ^            ")
+		console.log(ident + "         /–––––––––––     ")
+		console.log(ident + "    XOX ===       [] \\   ")
+		console.log(ident + "          \\           \\  ")
+		console.log(ident + "           \\___________]  ")
+		console.log(ident + "               I     I    ")
+		console.log(ident + "")
+
+		pause(50);
+
+		clear();
+		console.log(ident + "")
+		console.log(ident + "      :XXXX:XXXX:OOO:")
+		console.log(ident + "                 ^            ")
+		console.log(ident + "        X   /–––––––––––     ")
+		console.log(ident + "        O  ===       [] \\    ")
+		console.log(ident + "        X    \\           \\  ")
+		console.log(ident + "              \\___________]  ")
+		console.log(ident + "                  I     I    ")
+		console.log(ident + "")
+		pause(50);
+
+
+}
+
+function get_ident_string(num) {
+	let ident = "";
+	for(let i = 0; i < num; i++) {
+		ident += "         "
+	}
+	ident += "   "
+	return ident;
+
+
+}
+function pause(milliseconds) {
+	var dt = new Date();
+	while ((new Date()) - dt <= milliseconds) { /* Do nothing */ }
+}
